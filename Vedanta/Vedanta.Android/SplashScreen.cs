f0da323345e1aa096b87f511a7dbd1b62b0623d2
SureshChouhan <sureshkumar.c@inspireventures.in>
2022-03-10 00:08:44 +0530

    >Added Splash Screen

---
+++ Vedanta/Vedanta.Android/SplashScreen.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Vedanta.Droid
+{
+    [Activity(Label = "V-Gemba", Theme = "@style/MainTheme", //Indicates the theme to use for this activity
+            MainLauncher = true, //Set it as boot activity
+            NoHistory = true,//Doesn't place it in back stack
+       
+       ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Portrait)]
+    class SplashScreen : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            this.StartActivity(typeof(MainActivity));
+        }
+    }
+}
